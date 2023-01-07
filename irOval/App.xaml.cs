2ce0d048249677462d54f3ff110f12f292107917
Adam Heller <adam@heller.photo>
2023-01-07 13:40:17 -0500

    Add project files.

---
+++ irOval/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+
+namespace irOval
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        public void AppStart(object sender, StartupEventArgs e)
+        {
+            MainWindow mw = new MainWindow();
+            mw.Title = "This is a test";
+            mw.Show();
+        }
+
+        private void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            MessageBox.Show($"Something went wrong ({e.Exception.Message})", "Oopsie daisy", MessageBoxButton.OK, MessageBoxImage.Error);
+            e.Handled = true;
+        }
+    }
+}
---
+++ irOval/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using iRacingSDK;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace irOval
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        TextBlock throttleTextBlock;
+
+        private Label runningLabel;
+
+        iRacingConnection irc;
+
+        bool showIracingData = false;
+
+
+        void SetupIracingSdk()
+        {
+            irc = new iRacingConnection();
+            irc.NewSessionData += IracingNewData;
+        }
+
+        private void IracingNewData(DataSample data)
+        {
+            if (!showIracingData) return;
+
+            throttleTextBlock.Text = $"Throttle: {data.Telemetry.Throttle}%";
+
+        }
+
+        void SetupWidgets()
+        {
+            throttleTextBlock = (TextBlock)FindName("ThrottleTextBlock");
+
+            runningLabel = (Label)FindName("RunningLabel");
+
+        }
+
+        public MainWindow()
+        {
+            //AllowsTransparency="True"
+            //WindowStyle="None"
+            //Background="Transparent"
+            InitializeComponent();
+
+            SetupWidgets();
+
+            SetupIracingSdk();
+        }
+
+        private void StartButtonMouseClick(object sender, RoutedEventArgs e)
+        {
+            showIracingData = !showIracingData;
+            if (showIracingData)
+            {
+                runningLabel.Content = "Running";
+            }
+            else
+            {
+                runningLabel.Content = "Not running";
+            }
+        }
+    }
+}
